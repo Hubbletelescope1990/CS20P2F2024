6e6fe14bc76c06852c9c3888ec5937a8c48fc7be
38247004 <38247004@L815WD220024.edu.cbe.ab.ca>
2024-10-30 11:24:49 -0600

    aaa

--- Chapter7/src/Skillbuilders_Rectangle/TestRectangle.java
+++ Chapter7/src/Skillbuilders_Rectangle/TestRectangle.java
@@ -11,6 +11,15 @@ public class TestRectangle {
 		System.out.println("Rectangle area : " + spot.getArea());
 		System.out.println("Rectangle width : " + spot.getWidth());
 		
+Rectangle_part1of5 dot = new Rectangle_part1of5();
+		
+		dot.setLength(12);
+		dot.setWidth(8);
+		System.out.println("Rectangle length : " + dot.getLength());
+		System.out.println("Rectangle perimeter : " + dot.getPerimeter());
+		System.out.println("Rectangle area : " + dot.getArea());
+		System.out.println("Rectangle width : " + dot.getWidth());
+		
 
 	}
 
