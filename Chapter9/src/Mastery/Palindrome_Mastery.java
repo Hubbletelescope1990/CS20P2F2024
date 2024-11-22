d0020642406f2e4933f0921eb6ea14d3b8a5a06d
38247004 <38247004@L815WD220024.edu.cbe.ab.ca>
2024-11-22 10:48:13 -0700

    adfas

--- Chapter9/src/Mastery/Palindrome_Mastery.java
+++ Chapter9/src/Mastery/Palindrome_Mastery.java
@@ -25,39 +25,39 @@ public class Palindrome_Mastery {
 		phrase = phrase.toUpperCase();
 		phrase_array = phrase.toCharArray();
 		
-		for (int i = 0; i < phrase_array.length; i++) { //Counts letter occurrences
+		for (int i = 0; i < phrase_array.length; i++) { //counts amount of letters
 			//If statement checks if the character is a letter or not
 			if (phrase_array[i] >= LOW && phrase_array[i] <= HIGH) {
 				letter_count += 1;
 			}
 		}
 		
-		filtered_array = new int[letter_count];
+		filtered_array = new int[letter_count]; //Uses letter count to make appropriate space in array
 		
-		for (int i = 0; i < phrase_array.length; i++) {
+		for (int i = 0; i < phrase_array.length; i++) {// 
 			//If statement checks if the character is a letter or not
 			if (phrase_array[i] >= LOW && phrase_array[i] <= HIGH) {
-				filtered_array[letter_index] = phrase_array[i];
-				letter_index += 1;
+				filtered_array[letter_index] = phrase_array[i]; //Letter is placed into array
+				letter_index += 1;//Index increment
 			}
 		}
 		runtime = letter_count;
-		if (letter_count % 2 != 0) {
-			runtime -= 1;
+		if (letter_count % 2 != 0) { // If statement checks if there is an odd amount of letters
+			runtime -= 1; // Makes the final for loop run like the middle letter wasn't there
 		}
 		
-		runtime = runtime / 2;
+		runtime = runtime / 2;//The comparison only needs to run once per two letters
 		
-		for (int i = 0; i < runtime; i++) {
+		for (int i = 0; i < runtime; i++) { //Compares a letter to it's opposite
 			if (filtered_array[i] == filtered_array[(letter_count - reverse_index)]) {
 				char_checks += 1;
-				reverse_index += 1;
+				reverse_index += 1; //index increment
 			} else {
 				break;
 			}
 		}
 		
-		if (char_checks == runtime) {
+		if (char_checks == runtime) {//Checks if the for loop ran the full length
 			System.out.print("Word or phrase is a palindrome.");
 		} else {
 			System.out.print("Word or phrase is not a palindrome.");
