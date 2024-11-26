929924d93713c4dcf0dc734cf5f9ba85c1f4fb65
38247004 <38247004@L815WD220024.edu.cbe.ab.ca>
2024-11-26 09:38:02 -0700

    Chapter 9 complete. November 26 2024

--- Chapter9/src/Mastery/CourseGrades_Mastery.java
+++ Chapter9/src/Mastery/CourseGrades_Mastery.java
@@ -11,34 +11,25 @@ public class CourseGrades_Mastery {
 		Scanner userInput = new Scanner(System.in);
 		
 		int selection;
-		int student_number = 1;
-		int test_number = 1;
+		int student_number = 0;
+		int test_number = 0;
 		int grade = 0;
 		int student_selection;
 		int test_selection;
 		int quit = 0;
 		double average[][];
 		
-		System.out.print("Enter number of students");
+		System.out.print("Enter number of students: ");
 		student_number = userInput.nextInt();
-		System.out.print("Enter number of tests");
+		System.out.print("Enter number of tests: ");
 		test_number = userInput.nextInt();
+		System.out.println("");
 		
-		GradeBook_Mastery grades = new GradeBook_Mastery();
 		
-		/*
-		for (int i = 0; i < 60; i ++) {
-			System.out.print("Enter the grade for student " + student_number + " test " + test_number + ": ");
-			grade = userInput.nextInt();
-			scores.getGrades(student_number - 1, test_number - 1, grade);
-			test_number += 1;
-			if (test_number == 6) {
-				student_number += 1;
-				test_number = 1;
-			}
-		}
-		*/
+		GradeBook_Mastery grades = new GradeBook_Mastery(student_number, test_number);
+		
 		while (quit == 0) {
+			System.out.println("");
 			System.out.println("1: View all grades for all students");
 			System.out.println("2: View average grade for a student");
 			System.out.println("3: View average grade for a test");
@@ -48,14 +39,14 @@ public class CourseGrades_Mastery {
 			selection = userInput.nextInt();
 			
 			if (selection == 1) {
-				System.out.print();
+				grades.showGrades();
 				TimeUnit.SECONDS.sleep(2);
 			}
 			
 			if (selection == 2) {
 				System.out.print("Enter student number: ");
 				student_selection = userInput.nextInt();
-				System.out.print("Student " + student_selection + " average grade is: " + grades.studentAvg(student_selection - 1) + "%.");
+				System.out.print("The average grade of student " + student_selection + " is: " + grades.studentAvg(student_selection - 1) + "%.");
 				TimeUnit.SECONDS.sleep(2);
 			}
 			
@@ -74,7 +65,6 @@ public class CourseGrades_Mastery {
 				System.out.print("Invalid input, please try again");
 				TimeUnit.SECONDS.sleep(2);
 			}
-			System.out.println("");
 		}
 		
 	}
--- Chapter9/src/Mastery/GradeBook_Mastery.java
+++ Chapter9/src/Mastery/GradeBook_Mastery.java
@@ -1,12 +1,27 @@
 package Mastery;
 
+import java.util.Scanner;
+
 public class GradeBook_Mastery {
+	Scanner userInput = new Scanner(System.in);
 	
 	private double grades[][];
+	private int test;
+	private int student;
 	
 	//Constructor sets student and test numbers
 	public GradeBook_Mastery(int students, int tests) {
-
+		int grade;
+		student = students;
+		test = tests;
+		grades = new double[students][tests];
+		for (int i = 0; i < students; i ++) {
+			for (int a = 0; a < tests; a++) {
+				System.out.print("Enter the grade for student " + (i + 1) + " test " + (a + 1) + ": ");
+				grade = userInput.nextInt();
+				grades[i][a] = grade;
+			}
+		}
 	}
 	//Method changes the grade of a test for a specific student
 	public void getGrades(int student, int test, int grade) {
@@ -14,15 +29,30 @@ public class GradeBook_Mastery {
 	}
 	//Returns the entire array of grades
 	public void showGrades() {
+		for (int i = 0; i < student; i ++) {
+			System.out.print((i+1) + ": ");
+			for (int a = 0; a < test; a++) {
+				System.out.print(grades[i][a] + "% ");
+			}
+		System.out.println("");
+		}
 	}
 	//Returns the average grade of one student
 	public double studentAvg(int student) {
-		double average = (grades[student][0] + grades[student][1] + grades[student][2] + grades[student][3] + grades[student][4]) / 5;
+		double average = 0;
+		for (int i = 0; i < test; i++) {
+			average += grades[student][i];
+		}
+		average = average / test;
 		return(average);
 	}
 	//Returns the average grade of all students for one test
 	public double testAvg(int test) {
-		double average = (grades[0][test] + grades[1][test] + grades[2][test] + grades[3][test] + grades[4][test] + grades[5][test] + grades[6][test] + grades[7][test] + grades[8][test] + grades[9][test] + grades[10][test] + grades[11][test]) / 12;
+		double average = 0;
+		for (int i = 0; i < student; i++) {
+			average += grades[i][test];
+		}
+		average = average / student;
 		return(average);
 	}
 	
